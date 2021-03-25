eeaefabb32d9f361786e71046fee2f36220f7353
Tatyana Fomina <tatyana.fomina.1986@gmail.com>
2021-03-25 14:46:58 +0300

    Получение даты в прошлом и будущем

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
 
@@ -15,6 +17,12 @@ const options = yargs(hideBin(process.argv))
       alias: "d",
       describe: "Дата в календарном месяце",
     },
+    add: {
+      describe: "Получение даты в будущем",
+    },
+    sub: {
+      describe: "Получение даты в прошлом",
+    },
   })
   .help().argv;
 
@@ -28,5 +36,45 @@ if (options.month || options.m) {
 }
 if (options.date || options.d) {
   console.log(date.getDate());
+}
+
+// Получение даты в будущем
+if (options.add) {
+  if (options.year || options.y) {
+    console.log(
+      new Date(date.setFullYear(date.getFullYear() + options.y)).getFullYear()
+    );
+  }
+
+  if (options.month || options.m) {
+    console.log(
+      new Date(date.setMonth(date.getMonth() + options.m)).getMonth() + 1
+    );
+  }
+
+  if (options.date || options.d) {
+    console.log(new Date(date.setDate(date.getDate() + options.d)).getDate());
+  }
+}
+
+// Получение даты в прошлом
+if (options.sub) {
+  if (options.year || options.y) {
+    console.log(
+      new Date(date.setFullYear(date.getFullYear() - options.y)).getFullYear()
+    );
+  }
+
+  if (options.month || options.m) {
+    console.log(
+      new Date(date.setMonth(date.getMonth() - options.m)).getMonth() + 1
+    );
+  }
+
+  if (options.date || options.d) {
+    if (options.date || options.d) {
+      console.log(new Date(date.setDate(date.getDate() - options.d)).getDate());
+    }
+  }
 }
 console.log(date);
